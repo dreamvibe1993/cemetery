23c809ba6f0b186bc1b283a9dbc16aacc4329ca8
George Apraksin <dreamvibe@mail.ru>
2022-01-29 00:56:52 +0500

    code: no unused deps

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import "./App.css";
-import { Router, Routes, Route } from "react-router-dom";
 import { Paths } from "./Paths";
-import { Home } from "./pages/Home";
-import { createBrowserHistory } from "history";
-
-const history = createBrowserHistory();
 
 function App() {
-  return (
-    <Paths />
-  );
+  return <Paths />;
 }
 
 export default App;
--- src/Paths.js
+++ src/Paths.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Tomb } from "./pages/Tomb";
 import { Home } from "./pages/Home";
 
 export const Paths = () => {
-  console.log("rerender");
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/tomb" exact={true} element={<Tomb />} />
         <Route path="" exact={true} element={<Home />} />
-        <Route path="/home" exact={true} element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
