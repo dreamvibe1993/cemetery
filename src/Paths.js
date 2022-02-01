e37d08577a3df8fe0f62d51627a2958d37326c94
George Apraksin <dreamvibe@mail.ru>
2022-02-01 19:35:12 +0500

    code: added auth

--- src/Paths.js
+++ src/Paths.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Tomb } from "./pages/Tomb";
 import { Home } from "./pages/Home";
+import { UserAuth } from "./pages/UserAuth/UserAuth";
 
 export const Paths = () => {
   return (
     <Routes>
       <Route path="/tomb" exact={true} element={<Tomb />} />
       <Route path="/" exact={true} element={<Home />} />
+      <Route path="/auth" exact={true} element={<UserAuth />} />
     </Routes>
   );
 };
--- src/pages/UserAuth/UserAuth.js
+++ src/pages/UserAuth/UserAuth.js
@@ -1,6 +1,83 @@
 import React from "react";
 import styled from "styled-components/macro";
+import { ReactComponent as Logo } from "../../media/svg/logo.svg";
 
 export const UserAuth = () => {
-    return <div>auth</div>
-}
+  return (
+    <ContentContainer>
+      <UserAuthContainer>
+        <LogoWrapper>
+          <Logo />
+        </LogoWrapper>
+        <Title>Not authorized yet?</Title>
+        <LoginInput type="text" placeholder="login"></LoginInput>
+        <PasswordInput type="password" placeholder="password"></PasswordInput>
+        <LOGIN>LOGIN</LOGIN>
+      </UserAuthContainer>
+    </ContentContainer>
+  );
+};
+
+const ContentContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  width: 33%;
+  height: 40px;
+  background-color: #23a0b0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  user-select: none;
+  color: #fff;
+  border: none;
+`;
+
+const LOGIN = styled(Button)`
+    margin-top: 20px;
+`;
+
+const Input = styled.input`
+  width: 50%;
+  height: 40px;
+  background-color: rgba(0, 0, 0, 0.2);
+  border: none;
+  margin-bottom: 20px;
+  padding: 5px;
+  color: #fff;
+  font-size: 16px;
+`;
+
+const PasswordInput = styled(Input)``;
+
+const LoginInput = styled(Input)``;
+
+const LogoWrapper = styled.div`
+  width: 33%;
+  margin-bottom: 20px;
+  svg {
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 36px;
+  margin-bottom: 20px;
+`;
+
+const UserAuthContainer = styled.div`
+  padding: 40px 20px;
+  width: 900px;
+  margin: 10px auto;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
