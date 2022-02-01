0cde2478f759cad5888b301505c726ab86a36799
George Apraksin <dreamvibe@mail.ru>
2022-02-01 20:51:10 +0500

    code: added validation reg and red fields

--- src/models/yup/yup-schemas.js
+++ src/models/yup/yup-schemas.js
@@ -1,5 +1,7 @@
 import * as yup from "yup";
 
+//сообщения писать в required
+
 export const graveSchema = yup.object().shape({
   name: yup.string().required().min(2),
   dateB: yup.date().required(),
@@ -16,7 +18,29 @@ export const graveSchema = yup.object().shape({
 });
 
 export const giftSchema = yup.object().shape({
-  name: yup.string().required('name is requred').min(2).max(15),
-  gift: yup.string('choose a gift please').required('choose a gift please'),
+  name: yup.string().required("name is requred").min(2).max(15),
+  gift: yup.string("choose a gift please").required("choose a gift please"),
   wish: yup.string().max(30),
+});
+
+export const loginSchema = yup.object().shape({
+  loginEmail: yup.string().email().required("email is strongly required"),
+  loginPass: yup
+    .string()
+    .required("password is strongly required")
+    .min(5, "need a longer password")
+    .max(10, "easy. 5-15 symbols is enough"),
+});
+
+export const regSchema = yup.object().shape({
+  regPass: yup
+    .string()
+    .required("password is strongly required")
+    .min(5, "need a longer password")
+    .max(15, "easy. 5-15 symbols is enough"),
+  regUsername: yup
+    .string()
+    .required("please let us know how to address you")
+    .max(10, "easy. 0-10 symbols is enough"),
+  regEmail: yup.string().email().required("email is strongly required"),
 });
--- src/pages/UserAuth/UserAuth.js
+++ src/pages/UserAuth/UserAuth.js
@@ -1,12 +1,63 @@
 import React from "react";
 import styled from "styled-components/macro";
 import { ReactComponent as Logo } from "../../media/svg/logo.svg";
+import { regSchema } from "../../models/yup/yup-schemas";
 
 export const UserAuth = () => {
   const [isSignInOpen, setSignInOpen] = React.useState(false);
 
+  const [logEmail, setLogEmail] = React.useState("");
+  const [logPass, setLogPass] = React.useState("");
+
+  const [regEmail, setRegEmail] = React.useState("");
+  const [regPass, setRegPass] = React.useState("");
+  const [regUName, setRegUName] = React.useState("");
+
+  const [error, setError] = React.useState("");
+
+  const handleLoginEmailInput = (e) => {
+    console.log(e.target.value);
+    setLogEmail(e.target.value);
+  };
+  const handleLoginPasswordInput = (e) => {
+    console.log(e.target.value);
+    setLogPass(e.target.value);
+  };
+  const handleRegUsernameInput = (e) => {
+    console.log(e.target.value);
+    setRegUName(e.target.value);
+  };
+  const handleRegEmailInput = (e) => {
+    console.log(e.target.value);
+    setRegEmail(e.target.value);
+  };
+  const handleRegPasswordInput = (e) => {
+    console.log(e.target.value);
+    setRegPass(e.target.value);
+  };
+
   const openSignInSection = () => {
     setSignInOpen(true);
+  };
+
+  const signInUser = () => {
+    const toValidate = {
+      regPass: regPass,
+      regUsername: regUName,
+      regEmail: regEmail,
+    };
+    regSchema
+      .validate(toValidate)
+      .then((val) => {
+        console.log(val);
+      })
+      .catch((err) => {
+        console.error(err);
+        console.trace(err);
+        setError(err.path);
+        console.log(err.path);
+      });
+    console.log("signin");
   };
 
   return (
@@ -16,27 +67,61 @@ export const UserAuth = () => {
           <Logo />
         </LogoWrapper>
         <Title>Not authorized yet?</Title>
-        <LoginInput type="email" placeholder="e-mail"></LoginInput>
-        <PasswordInput type="password" placeholder="password"></PasswordInput>
+        <LoginInput
+          type="email"
+          placeholder="e-mail"
+          required
+          onChange={(e) => handleLoginEmailInput(e)}
+          err={error === "loginEmail"}
+        ></LoginInput>
+        <PasswordInput
+          type="password"
+          placeholder="password"
+          required
+          onChange={(e) => handleLoginPasswordInput(e)}
+          err={error === "loginPass"}
+        ></PasswordInput>
         <LOGIN>LOGIN</LOGIN>
         <SignInSection open={isSignInOpen}>
-          <LoginInput type="email" placeholder="e-mail"></LoginInput>
-          <PasswordInput type="password" placeholder="password"></PasswordInput>
+          <LoginInput
+            type="text"
+            placeholder="username"
+            required
+            onChange={(e) => handleRegUsernameInput(e)}
+            err={error === "regUsername"}
+          ></LoginInput>
+          <LoginInput
+            type="email"
+            placeholder="e-mail"
+            required
+            onChange={(e) => handleRegEmailInput(e)}
+            err={error === "regEmail"}
+          ></LoginInput>
+          <PasswordInput
+            type="password"
+            placeholder="password"
+            required
+            onChange={(e) => handleRegPasswordInput(e)}
+            err={error === "regPass"}
+          ></PasswordInput>
         </SignInSection>
-        <SIGNIN onClick={openSignInSection}>SIGN IN</SIGNIN>
+        <SIGNIN onClick={isSignInOpen ? signInUser : openSignInSection}>
+          SIGN IN
+        </SIGNIN>
       </UserAuthContainer>
     </ContentContainer>
   );
 };
 
 const SignInSection = styled.div`
-  transition: height 0.3s linear, opacity 0.3s 0.4s linear;
+  transition: height 0.3s linear, padding-top 0.3s linear,
+    opacity 0.3s 0.4s linear;
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
-  padding-top: 40px;
-  height: ${(p) => (p.open ? "160px" : "0px")};
+  padding-top: ${(p) => (p.open ? "40px" : "0px")};
+  height: ${(p) => (p.open ? "220px" : "0px")};
   opacity: ${(p) => (p.open ? "1" : "0")};
   overflow: hidden;
 `;
@@ -77,6 +162,7 @@ const Input = styled.input`
   padding: 5px 10px;
   color: #fff;
   font-size: 16px;
+  border: ${(p) => (p.err ? "1px solid rgba(168, 50, 50,.9)" : "none")};
 `;
 
 const PasswordInput = styled(Input)``;
